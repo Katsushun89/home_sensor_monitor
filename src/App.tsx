2393716941d11669a45a2136e612496afb3a71c9
Katsushun89 <Katsushun89@gmail.com>
2022-03-22 01:04:39 +0900

    Add authenticator in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Charts from './Charts';
+import { Amplify } from "aws-amplify";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+    <Authenticator>
+        {({ signOut, user }) => (
+        <div className="App">
+            <header className="App-header">
+            <h1 className="Title">home-sensor-monitor</h1>
+              </header>
+              <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
+);
 }
 
 export default App;
